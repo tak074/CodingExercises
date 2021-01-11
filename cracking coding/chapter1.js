4f405c44e4b80c367d0f884c6b6ca2b582f74b1e
root <tak074@ucsd.edu>
2021-01-10 18:27:18 -0800

    exercises

--- cracking coding/chapter1.js
+++ cracking coding/chapter1.js
@@ -11,6 +11,84 @@ var allUnique = function (str) {
   return unique;
 }
 
-let sample = 'strings';
+// Q2
+var checkPermutation = function(str1, str2) {
+  str1 = str1.sort();
+  str2 = str2.sort();
+  return str1 === str2;
+}
 
-console.log(allUnique(sample));
+// Q3
+var substitute = function(str, part) {
+  return str = str.split(' ').join('%20');
+}
+
+// Q4
+var palindromePermutation = function(str) {
+  let palindrome = true;
+  let counter = 0;
+
+  while (counter < str.length && palindrome) {
+    let substring = str.slice(i + 1);
+    if (substring.indexOf(str[i]) === -1) {
+      if (palindrome === true) {
+        palindrome = 1;
+      } else {
+        palindrome = false;
+      }
+    }
+    counter++;
+  }
+}
+
+// Q5
+var oneEdit = function(str1, str2) {
+  let lenDiff = Math.abs(str1.length - str2.length);
+  if (lenDiff > 1) return false;
+  else if (lenDiff === 1) {
+    let longer = str1.length > str2.length? str1 : str2;
+    let shorter = str1.length < str2.length? str1 : str2;;
+    longer = longer.sort();
+    shorter = shorter.sort();
+
+    for (let i = 0; i < shorter.length; i++) {
+      if (longer[i] !== shorter[i]) {
+        return longer.slice(i + 2) === shorter.slice(i + 1);
+      }
+    }
+  } else {
+    str1 = str1.sort();
+    str2 = str2.sort();
+
+    for (let i = 0; i < str1.length; i++) {
+      if (str1[i] !== str2[i]) {
+        return str1.slice(i + 1) === str2.slice(i + 1);
+      }
+    }
+  }
+  return true;
+}
+
+// Q6
+var compress = function(string) {
+  let compressed = '';
+  let compressor = function(str) {
+    let firstChar = str[0];
+    let counter = 1;
+    while (str[counter] === firstChar) {
+      counter++;
+    }
+
+    let remainder = str.slice(counter);
+    if (remainder.length !== 0) {
+      compressor(remainder);
+    }
+    compressed = firstChar + counter + compressed;
+  }
+
+  compressor(string);
+
+  return compressed.length < string.length? compressed : string;
+}
+
+// Q7
