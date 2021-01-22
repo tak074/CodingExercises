54245453de2e1409c60ce489ffec733620c74eee
root <tak074@ucsd.edu>
2021-01-21 17:23:00 -0800

    created dictionary to skip already used number within that loop. reduced runtime from 400ms to 100ms.

--- leetCode/permutations2.js
+++ leetCode/permutations2.js
@@ -1,20 +1,22 @@
 var permuteUnique = function(nums) {
   if (nums.length === 1) return [nums];
 
-  let check = {};
+  // let check = {};
   let solutions = [];
+  let numsUsed = {};
 
   for (let i = 0; i < nums.length; i++) {
-    let currNum = [nums[i]];
+    let currNum = nums[i];
+    if (numsUsed[currNum]) continue;
+    numsUsed[currNum] = true;
+    currNum = [currNum];
     let newNum = (nums.slice(0, i)).concat(nums.slice(i + 1));
     let others = permuteUnique(newNum);
     others.forEach((comb) => {
       let newComb = currNum.concat(comb);
       let key = newComb.join('');
-      if (!check[key]) {
-        solutions.push(newComb);
-      }
-      check[key] = true;
+      solutions.push(newComb);
+
     })
   }
 
