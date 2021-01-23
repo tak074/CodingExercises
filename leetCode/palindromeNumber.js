857ecb7db861b81e1064528dc40c407d025f7d56
root <tak074@ucsd.edu>
2021-01-22 18:18:50 -0800

    optimized palindrome Number. Reduced runtime from 228ms to 184ms, faster than 99.54%

--- leetCode/palindromeNumber.js
+++ leetCode/palindromeNumber.js
@@ -5,18 +5,35 @@ var isPalindrome = function(x) {
   let reverse = str.split('').reverse().join('');
 
   return str === reverse;
+};
 
-  // let left = 0;
-  // let right = x.length - 1;
-  // let res = true;
-  // while (left < right) {
-  //   if (x[left] !== x[right]) {
-  //     res = false;
-  //     break;
-  //   }
-  //   left++;
-  //   right--;
-  // }
+var isPalindrome = function(x) {
+  if (x < 0) return false;
 
-  // return res;
+  let left = 0;
+  let right = x.length - 1;
+  let res = true;
+  while (left < right) {
+    if (x[left] !== x[right]) {
+      res = false;
+      break;
+    }
+    left++;
+    right--;
+  }
+
+  return res;
+};
+
+// fastest
+var isPalindrome = function(x) {
+  let num = x;
+  let rev = 0;
+
+  while (num > 1) {
+    rev = (rev * 10) + num % 10;
+    num = Math.floor(num / 10);
+  }
+
+  return x === rev;
 };
