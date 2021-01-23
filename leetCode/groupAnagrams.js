40028da111c36c59b40a793428811564826e1966
root <tak074@ucsd.edu>
2021-01-22 19:55:07 -0800

    optimization by associating different prime number to each characters. reduced run time to 116ms, faster than 96.62%

--- leetCode/groupAnagrams.js
+++ leetCode/groupAnagrams.js
@@ -14,3 +14,20 @@ var groupAnagrams = function(strs) {
 
   return Object.values(anagrams);
 };
+
+
+var groupAnagrams = function (strs) {
+  let anagrams = {};
+  const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101];
+
+  strs.forEach((word) => {
+    let key = word.split('').reduce((acc, curr) => acc * primes[curr.charCodeAt() - 97], 1);
+
+    if (!anagrams[key]) {
+      anagrams[key] = [];
+    }
+    anagrams[key].push(word);
+  })
+
+  return Object.values(anagrams);
+};
