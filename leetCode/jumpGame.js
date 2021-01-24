216038f3bb524652c915817cf927f7e159baf77e
root <tak074@ucsd.edu>
2021-01-23 18:33:14 -0800

    implemented jumpGame through Greedy Algorithm. could be optimized further

--- leetCode/jumpGame.js
+++ leetCode/jumpGame.js
@@ -1,21 +1,12 @@
 var canJump = function(nums) {
   if (nums.length === 1) return true;
-  let valid = false;
+  let max = 0;
 
-  // find places that can get to target
   for (let i = 0; i < nums.length - 1; i++) {
-    // if they can jump to the last index
-    if (nums[i] + i >= nums.length - 1) {
-      // if their index !== 0
-      if (i !== 0) {
-        // recurse as that location as the target
-        valid = canJump(nums.slice(0, i + 1));
-        if (valid) break;
-      } else {
-        return true;
-      }
-    }
+    max = Math.max(max, nums[i] + i);
+    if (max <= i) return false;
+    if (max >= nums.length - 1) return true;
   }
 
-  return valid;
+  return false;
 };
