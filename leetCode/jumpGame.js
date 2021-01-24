58f68fd439c68bf3b8d60fe2b0b93d917fbdbfb2
root <tak074@ucsd.edu>
2021-01-23 18:39:55 -0800

    reduced the number of computations. reduced runtime from 84ms to 76ms, faster than 92.39%

--- leetCode/jumpGame.js
+++ leetCode/jumpGame.js
@@ -10,3 +10,15 @@ var canJump = function(nums) {
 
   return false;
 };
+
+var canJump = function(nums) {
+  if (nums.length === 1) return true;
+  let max = 0;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    max = Math.max(max, nums[i] + i);
+    if (max <= i) return false;
+  }
+
+  return max >= nums.length - 1;
+};
