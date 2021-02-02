73edafa4389638124f052db0ecd1d16d5eaa4470
root <tak074@ucsd.edu>
2021-02-01 17:25:14 -0800

    reimplemented with optimized space top 90% in space usage

--- leetCode/uniquePath.js
+++ leetCode/uniquePath.js
@@ -3,15 +3,14 @@ var uniquePaths = function(m, n) {
     return 1;
   }
 
-  let row = new Array(n).fill(1);
-  let matrix = new Array(m).fill(row);
+  let matrix = new Array(n).fill(1);
 
   console.log(matrix);
   for (let i = m - 2; i >= 0; i--) {
     for (let j = n - 2; j >= 0; j--) {
-      matrix[i][j] = matrix[i + 1][j] + matrix[i][j + 1];
+      matrix[j] = matrix[j] + matrix[j + 1];
     }
   }
 
-  return matrix[0][0];
+  return matrix[0];
 };
