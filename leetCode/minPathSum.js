304c1741ccc6e7f7a79a73134bd9dab01a9701a8
root <tak074@ucsd.edu>
2021-02-02 15:07:39 -0800

    reduced runtime by implementing it with greedy algorithm. runtime: 64ms, faster than 100%, memory used is less than 90.72%

--- leetCode/minPathSum.js
+++ leetCode/minPathSum.js
@@ -40,3 +40,30 @@ var minPathSum = function(grid) {
 
   return smallestSum;
 };
+
+
+var minPathSum = function(grid) {
+  // each path is the sum of previous steps and current square.\
+  // each square will stem from the square above or to the left.
+  // the smaller sum will be that square's value.
+
+  let r = grid.length;
+  let c = grid[0].length;
+
+  for (let i = 0; i < r; i++) {
+    for (let j = 0; j <c; j++) {
+      //skip the first square.
+      if (i > 0 && j > 0) {
+        grid[i][j] = Math.min(grid[i][j] + grid[i - 1][j], grid[i][j] + grid[i][j - 1]);
+      } else if (i > 0 || j > 0) {
+        if (i > 0) {
+          grid[i][j] = grid[i][j] + grid[i - 1][j];
+        } else {
+          grid[i][j] = grid[i][j] + grid[i][j - 1];
+        }
+      }
+    }
+  }
+
+  return grid[r - 1][c - 1];
+};
