32ed2b46bd540004592b4a1e3df1df6087488da2
root <tak074@ucsd.edu>
2021-02-05 13:54:36 -0800

    implemented uniquePathWithObstacles. runtime 72ms, faster than 98.41%

--- leetCode/uniquePathII.js
+++ leetCode/uniquePathII.js
@@ -1,35 +1,25 @@
 var uniquePathsWithObstacles = function(obstacleGrid) {
   let m = obstacleGrid.length;
   let n = obstacleGrid[0].length;
-  if (obstacleGrid[m - 1][n - 1] === 1) {
-    return 0;
+  if (m === 1 && n === 1) {
+    return obstacleGrid[0][0] === 1? 0 : 1;
   }
 
-  let matrix = [];
-  for (let x = 0; x < m; x++) {
-      matrix.push([]);
-      for (let y = 0; y < n; y++) {
-          matrix[x][y] = 1;
-      }
-  }
+  let matrix = new Array(n).fill(0);
 
   for (let i = m - 1; i >= 0; i--) {
     for (let j = n - 1; j >= 0; j--) {
       if (obstacleGrid[i][j] === 1) {
-        matrix[i][j] = 0;
+        matrix[j] = 0;
+        continue;
+      } else if (i === m - 1 && j === n - 1) {
+        matrix[j] = 1;
         continue;
       }
-      if (i === m - 1 || j === n - 1) {
-        if (matrix[i + 1][j] === 0 || matrix[i][j + 1] === 0) {
-          matrix[i][j] = 0;
-        } else {
-          matrix[i][j] = 1;
-        }
-      } else {
-        matrix[i][j] = matrix[i + 1][j] + matrix[i][j + 1];
-      }
+      let right = matrix[j + 1]? matrix[j + 1] : 0;
+      let down = matrix[j]? matrix[j] : 0;
+      matrix[j] = right + down;
     }
   }
-
-  return matrix[0][0];
+  return matrix[0];
 };
