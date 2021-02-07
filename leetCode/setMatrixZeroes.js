6187bdcf4f3dad4e1699bf5a3ac37b5efc8f53a2
root <tak074@ucsd.edu>
2021-02-07 14:01:13 -0800

    reduced space complexity to 40.1mb, less than 96.27%

--- leetCode/setMatrixZeroes.js
+++ leetCode/setMatrixZeroes.js
@@ -19,3 +19,23 @@ var setZeroes = function(matrix) {
   // return the changed matrix
   return matrix;
 };
+
+var setZeroes = function(matrix) {
+  let rows = new Set();
+  let cols = new Set();
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (!matrix[i][j]) {
+        rows.add(i);
+        cols.add(j);
+      }
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (rows.has(i) || cols.has(j)) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+};
