e8997de659c7db8c778e3dba3ccfcfad26e6bb00
root <tak074@ucsd.edu>
2021-02-11 14:41:39 -0800

    got the algorithm to pass the time limit, but still can be optimized further

--- leetCode/wordSearch.js
+++ leetCode/wordSearch.js
@@ -2,30 +2,29 @@ var exist = function(board, word) {
 
   // loop through each letter to find the first letter.
   for (let i = 0; i < board.length; i++) {
-    let firstLet = word[0];
-    if (board[i].indexOf(firstLet) === -1) continue;
     for (let j = 0; j <board[0].length; j++) {
       // if found the match for the first letter
-
-      let checkProximity = function(row, col, index, matrix) {
+      let checkProximity = function(row, col, index) {
         let currChar = word[index];
-        let tile = matrix[row][col];
+        let tile = board[row][col];
         // if it's a match
         if (tile === currChar) {
-          let newMatrix = matrix.map(arr => arr.slice());
           // remove from the matrix to avoid revisits
-          newMatrix[row][col] = null;
+          board[row][col] = null;
           // if it's the last letter
           if (index === word.length - 1) {
             return true;
           } else {
             // move on to the next letter
-            let left = newMatrix[row][col - 1]? checkProximity(row, col - 1, index + 1, newMatrix) : null;
-            let right = newMatrix[row][col + 1]? checkProximity(row, col + 1, index + 1, newMatrix) : null;
-            let top = newMatrix[row - 1]? checkProximity(row - 1, col, index + 1, newMatrix) : null;
-            let bottom = newMatrix[row + 1]? checkProximity(row + 1, col, index + 1, newMatrix) : null;
+            let left = board[row][col - 1]? checkProximity(row, col - 1, index + 1) : null;
+            let right = board[row][col + 1]? checkProximity(row, col + 1, index + 1) : null;
+            let top = board[row - 1]? checkProximity(row - 1, col, index + 1) : null;
+            let bottom = board[row + 1]? checkProximity(row + 1, col, index + 1) : null;
 
-            return left || right || top || bottom;
+            if (left || right || top || bottom) {
+              return true;
+            }
+            board[row][col] = word[index];
           }
         }
 
@@ -33,7 +32,7 @@ var exist = function(board, word) {
       }
       // call the function
       // if the function returns true,
-      if (checkProximity(i, j, 0, board)) {
+      if (checkProximity(i, j, 0)) {
         // return true
         return true;
       }
