c3a5ec244e86c8596bf4f358e3d8317073d2f5b4
root <tak074@ucsd.edu>
2021-02-11 15:22:39 -0800

    implemented removeDuplicates through greedyAlgorithm. since nums were sorted, instead of keeping library of nums, checked how many time a certain number repeated and removed and relocated pointer accordingly. runtime 80ms, faster than 97.33%

---
+++ leetCode/removeDuplicates.js
@@ -0,0 +1,27 @@
+var removeDuplicates = function(nums) {
+  // loop through each num
+  for (let i = 0; i < nums.length - 1; i++) {
+    let currNum = nums[i];
+    let nextNum = nums[i + 1];
+    let loc = i;
+    let counter = 0;
+
+    while (currNum === nextNum) {
+      counter++;
+      i++;
+      nextNum = nums[i + 1];
+    }
+
+    // at this point, i + 1 should be a different number.
+    // and counter equals the num of duplicates.
+    // if counter === 1, it's fine, since we should allow a num to show up to twice
+    // but if counter > 1
+    if (counter > 1) {
+      // remove through splice. counter - 1 times.
+      nums.splice(loc, counter - 1);
+      i -= counter - 1;
+    }
+  }
+
+  return nums.length;
+};
