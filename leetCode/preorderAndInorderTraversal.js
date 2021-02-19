fe646699fc0dc135306a4c9b74a8d7e3a586f7e3
root <tak074@ucsd.edu>
2021-02-19 12:22:14 -0800

    implemented preorderAndInorderTraversal. using preorder as guide for sequence and inorder as immediate node groupings. runtime 164ms, faster than 33.49. needs to improve time complexity%

--- leetCode/preorderAndInorderTraversal.js
+++ leetCode/preorderAndInorderTraversal.js
@@ -4,16 +4,25 @@ function TreeNode(val, left, right) {
   this.right = (right===undefined ? null : right);
 }
 
-var buildTree = function(preorder, inorder) {
-  // first number of preorder is the head.
-  let head = new TreeNode(preorder[0]);
+var buildTree = function(pre, inorder) {
   // preorder groups things by level, but doesn't specify who gets connected to who
   // inorder groups things by immediate connection.
-
-  let inorderToTree = function(arr) {
-    if (arr.length === 1) return new TreeNode(arr[0]);
-    else {
-      let parent = new TreeNode(arr[Math.floor(arr.length / 2)])
-    }
+  if (inorder.length === 1) return new TreeNode(inorder[0]);
+  if (inorder.length === 0) return null;
+  // first number from pre that is has index !== -1 is the root in inorder.
+  let index = 0;
+  let parentIndex = inorder.indexOf(pre[index]);
+  while (parentIndex === -1) {
+    index++;
+    parentIndex = inorder.indexOf(pre[index]);
   }
+  // pre[index] is the root of this in.
+  let parent = new TreeNode(pre[index]);
+  let left = buildTree(pre.slice(index + 1), inorder.slice(0, parentIndex));
+  let right = buildTree(pre.slice(index + 1), inorder.slice(parentIndex + 1));
+
+  parent.left = left;
+  parent.right = right;
+
+  return parent;
 };
