54ff82fe520347441c3573d020ee6d69f07ea71d
root <tak074@ucsd.edu>
2021-02-22 15:20:55 -0800

    improved time complexity through greedy algorithm. runtime 84ms, faster than 59.57%

--- leetCode/gasStation.js
+++ leetCode/gasStation.js
@@ -1,35 +1,17 @@
 var canCompleteCircuit = function(gas, cost) {
-  let res = -1;
-
-  // a function that calculates traveling.
-  let moreGasThanCost = function(index, gas, cost) {
-    let counter = 0;
-    let path = gas.length;
-    let fuel = gas[index];
-    let burn = cost[index];
-
-    // use mode since circular path
-    while (counter < path) {
-      // if breaks, return false;
-      if (fuel < burn) return false;
-      counter++;
-      index++;
-      fuel += gas[index % path];
-      burn += cost[index % path];
-    }
-    return true;
-  }
+  let res = 0;
+  let totalGas = 0;
+  let currGas = 0;
 
   // iterate through to find possible start point.
   for (let i = 0; i < gas.length; i++) {
-    if (gas[i] >= cost[i]) {
-      // function to check
-      if (moreGasThanCost(i, gas, cost)) {
-        res = i;
-        return res;
-      }
+    currGas += gas[i] - cost[i];
+    totalGas += gas[i] - cost[i];
+    if (currGas < 0) {
+      currGas = 0;
+      res = i + 1;
     }
   }
 
-  return res;
+  return totalGas < 0? -1 : res;
 };
