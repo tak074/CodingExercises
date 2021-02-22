849e457e8128f45f8c47a49cf0dfe21497e0315e
tak074 <tak074@ucsd.edu>
2021-02-22 15:22:55 -0800

    testing loging in

--- leetCode/gasStation.js
+++ leetCode/gasStation.js
@@ -2,7 +2,6 @@ var canCompleteCircuit = function(gas, cost) {
   let res = 0;
   let totalGas = 0;
   let currGas = 0;
-
   // iterate through to find possible start point.
   for (let i = 0; i < gas.length; i++) {
     currGas += gas[i] - cost[i];
@@ -12,6 +11,5 @@ var canCompleteCircuit = function(gas, cost) {
       res = i + 1;
     }
   }
-
   return totalGas < 0? -1 : res;
 };
