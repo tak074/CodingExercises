621d8fc62d579edd1b716259eb4637c3e4ac5f3d
tak074 <tak074@ucsd.edu>
2021-02-23 10:30:51 -0800

    implemented LRUCache using hashmap and its built in keys().next().value feature. runtime 184ms, faster than 92.78%

---
+++ leetCode/LRUCache.js
@@ -0,0 +1,31 @@
+var LRUCache = function(capacity) {
+  this.capacity = capacity;
+  this.map = new Map();
+};
+
+// /**
+//  * @param {number} key
+//  * @return {number}
+//  */
+LRUCache.prototype.get = function(key) {
+  if (!this.map.has(key)) return -1;
+  // update the order
+  let value = this.map.get(key);
+  this.map.delete(key);
+  this.map.set(key, value);
+  return value;
+};
+
+// /**
+//  * @param {number} key
+//  * @param {number} value
+//  * @return {void}
+//  */
+LRUCache.prototype.put = function(key, value) {
+  if (this.map.has(key)) this.map.delete(key);
+  this.map.set(key, value);
+  if (this.map.size > this.capacity) {
+    // erase the first item
+    this.map.delete(this.map.keys().next().value);
+  }
+};
