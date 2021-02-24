1dd50d3d4837cb240973df2d78ad8e0b8f5d2242
tak074 <tak074@ucsd.edu>
2021-02-23 17:18:37 -0800

    improved time complexity through greedy algorithm. runtime 99ms, faster than 51.38%

--- leetCode/maximumProductSubarray.js
+++ leetCode/maximumProductSubarray.js
@@ -2,15 +2,15 @@ var maxProduct = function(nums) {
   if (nums.length === 0) return null;
   if (nums.length === 1) return nums[0];
 
-  let maxes = [];
-  for (let i = 0; i < nums.length; i++) {
-    let currProd = nums[i];
-    let currMaxArr = [currProd];
-    for (let j = i + 1; j < nums.length; j++) {
-      currProd *= nums[j];
-      currMaxArr.push(currProd);
-    }
-    maxes[i] = Math.max(...currMaxArr);
+  let maxes = [nums[0]];
+  nums[0] = [nums[0], nums[0]];
+  for (let i = 1; i < nums.length; i ++) {
+    let currNum = nums[i];
+    let products = [currNum];
+    products.push(nums[i - 1][0] * currNum);
+    products.push(nums[i - 1][1] * currNum);
+    nums[i] = [Math.min(...products), Math.max(...products)];
+    maxes.push(Math.max(...products));
   }
 
   return Math.max(...maxes);
