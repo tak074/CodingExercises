4724f8dd05d976485b63dac0703b4639f97de900
tak074 <tak074@ucsd.edu>
2021-02-24 19:24:29 -0800

    simple whiteboarding session and leetcode set up

---
+++ leetCode/BSTIterator.js
@@ -0,0 +1,17 @@
+var BSTIterator = function(root) {
+
+};
+
+/**
+ * @return {number}
+ */
+BSTIterator.prototype.next = function() {
+
+};
+
+/**
+ * @return {boolean}
+ */
+BSTIterator.prototype.hasNext = function() {
+
+};
