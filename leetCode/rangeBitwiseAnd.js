8ed6fb7e9832627e7d93111b8bc1573e8fe02edd
tak074 <tak074@ucsd.edu>
2021-02-25 12:02:54 -0800

    tried implement through bitwise method but is slower

--- leetCode/rangeBitwiseAnd.js
+++ leetCode/rangeBitwiseAnd.js
@@ -22,3 +22,24 @@ var rangeBitwiseAnd = function(m, n) {
 
   return parseInt(res, 2);
 };
+
+var rangeBitwiseAnd = function(m, n) {
+  m = m.toString(2);
+  n = n.toString(2);
+  while (m.length !== n.length) {
+    if (m.length < n.length) {
+      m = '0' + m;
+    } else {
+      n = '0' + n;
+    }
+  }
+
+  let res = '';
+  let i = 0;
+  while (m[i] === n[i]) {
+    res += m[i];
+    i++;
+  }
+
+  return res << m.length - i;
+};
