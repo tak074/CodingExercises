2e07e8beff7608965120af0b1959b545b6a0880b
tak074 <tak074@ucsd.edu>
2021-02-25 08:28:18 -0800

    implemented BSTIterator by keeping track of the order and the current index as its property. runtime 124ms, faster than 99.79%

--- leetCode/BSTIterator.js
+++ leetCode/BSTIterator.js
@@ -1,17 +1,30 @@
 var BSTIterator = function(root) {
+  this.index = -1;
+  this.order = [];
 
+  let temp = [];
+  let recordOrder = function(node) {
+    if (!node) return null;
+
+    recordOrder(node.left);
+    temp.push(node.val);
+    recordOrder(node.right);
+  }
+  recordOrder(root);
+  this.order = temp;
 };
 
 /**
  * @return {number}
  */
 BSTIterator.prototype.next = function() {
-
+  this.index++;
+  return this.order[this.index];
 };
 
 /**
  * @return {boolean}
  */
 BSTIterator.prototype.hasNext = function() {
-
+  return !!this.order[this.index + 1] || this.order[this.index + 1] === 0;
 };
