97cc59849a534c2c164fa226bac508fcd4029dc5
tak074 <taeksukim625@gmail.com>
2021-02-25 20:55:42 -0800

    improved the time complexity by maintaining more consistent window size. runtime 76ms, faster than 89.11%

--- leetCode/minSizeSubarraySum.js
+++ leetCode/minSizeSubarraySum.js
@@ -1,20 +1,16 @@
 var minSubArrayLen = function(target, nums) {
     let minLen = Infinity;
 
+    let left = 0;
+    let currSum = 0;
     // iterate through nums for different starting points
-    for (let i = 0; i < nums.length; i++) {
-        let lenCounter = 1;
-        let currSum = 0;
-        let index = i;
-        // iterate through the rest of the array until currSum >= target 
-        while (nums[index] !== undefined) {
-            currSum += nums[index];
-            if (currSum >= target) {
-                minLen = Math.min(minLen, lenCounter);
-                break;
-            }
-            index++;
-            lenCounter++;
+    for (let right = 0; right < nums.length; right++) {
+        currSum += nums[right];
+
+        while (currSum >= target) {
+            minLen = Math.min(minLen, right - left + 1);
+            currSum -= nums[left];
+            left++;
         }
     }
 
