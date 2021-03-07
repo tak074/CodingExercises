09ed20b59edfdc96a295b6ef90c1827f7457d3af
tak074 <tak074@ucsd.edu>
2021-03-06 17:34:42 -0800

    implemented through dynamic programming. o(n * m);

--- leetCode/maximalSquare.js
+++ leetCode/maximalSquare.js
@@ -1,35 +1,15 @@
 var maximalSquare = function(matrix) {
   let max = 0;
-  let res = new Array(matrix[0].length).fill([0,0]);
+  let dp = new Array(matrix.length + 1).fill(0).map(() => row = new Array(matrix[0].length + 1).fill(0));
 
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[0].length; j++) {
-      if (matrix[i][j] === '0') {
-        max = Math.max(max, (Math.min(...res[j]))**2);
-        res[j] = [0,0];
-      } else {
-        let counter = 0;
-        while (matrix[i][j] === '1') {
-          counter++;
-          j++;
-        }
-        // counter will represent number of consecutive 1's.
-        while (counter > 0) {
-          if (res[j - counter][1] !== counter) {
-            max = Math.max(max, Math.min(...res[j - counter])**2);
-            res[j - counter] = [1, counter];
-          } else {
-            res[j - counter][0]++;
-          }
-          if (i === matrix.length - 1) {
-            max = Math.max(max, Math.min(...res[j - counter])**2);
-          }
-          counter--;
-        }
-        j--;
+  for (let i = 1; i < matrix.length + 1; i++) {
+    for (let j = 1; j < matrix[0].length + 1; j++) {
+      if (matrix[i - 1][j - 1] === '1') {
+        dp[i][j] = Math.min(dp[i - 1][j],dp[i - 1][j - 1], dp[i][j - 1]) + 1;
+        max = Math.max(max, dp[i][j]);
       }
     }
   }
 
-  return max;
+  return max**2;
 };
