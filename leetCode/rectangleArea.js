563961d5f3f99c0e8c50466ec5a3de4408fed656
tak074 <tak074@ucsd.edu>
2021-03-07 16:06:11 -0800

    cleaned up the code

--- leetCode/rectangleArea.js
+++ leetCode/rectangleArea.js
@@ -1,19 +1,11 @@
 var computeArea = function(A, B, C, D, E, F, G, H) {
-  // D > B, H > F
-  // if (D > H) commonHeight = H - B;
-  // else commonHeight = D - F;
-
   let area1 = (C - A) * (D - B);
   let area2 = (G - E) * (H - F);
 
-  if (C <= E || G <= A || D <=F || H <= B) return area1 + area2;
+  let commonHeight = Math.min(D, H) - Math.max(B, F);
+  let commonWidth = Math.min(C, G) - Math.max(A, E);
+  commonHeight = commonHeight > 0? commonHeight : 0;
+  commonWidth = commonWidth > 0? commonWidth : 0;
 
-  let top = D > H? H : D;
-  let bottom = B > F? B : F;
-  let right = C > G? G : C;
-  let left = A > E? A : E;
-
-  let commonArea = (top - bottom) * (right - left);
-
-  return area1 + area2 - commonArea;
+  return area1 + area2 - (commonHeight * commonWidth);
 };
