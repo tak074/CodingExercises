97edcd50c92343fdf818a59c877afde226dd3293
tak074 <tak074@ucsd.edu>
2021-03-09 12:08:12 -0800

    implementing kThSmallest through LinkedList and through a simple counter method. finished counter methodthod. runtime 84ms faster than 95.98%

---
+++ leetCode/KthSmallestElement.js
@@ -0,0 +1,50 @@
+// LinkedList method
+let LinkedList = function(val = 0, next = null) {
+  this.val = val;
+  this.next = next;
+}
+
+var kthSmallest = function(root, k) {
+  // create a LinkedList through recursion.
+
+  let head = new LinkedList;
+  let prev = head;
+
+  prev.next = new LinkedList(root.val);
+  let curr = prev.next;
+
+  let createChain = function(node, prev, curr) {
+    if (node.left) {
+      // create the node
+      let newNode = new LinkedList(node.left.val);
+      // connect it between prev and curr.
+      newNode.next = curr;
+      prev.next = newNode;
+      createChain(node.left, prev, newNode);
+    }
+    if (node.right)
+  }
+
+  createChain(root);
+};
+
+// simple counter method
+var kthSmallest = function(root, k) {
+  let counter = 0;
+  let searching = true;
+  let res = null;
+  let traverse = function(node) {
+    if (!searching) return;
+    if (!node) return;
+
+    traverse(node.left);
+    counter++;
+    if (counter === k) {
+      searching = false;
+      res = node.val;
+    }
+    traverse(node.right);
+  }
+
+  return res;
+};
