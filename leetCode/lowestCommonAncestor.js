42c9163a6d72cf08a125ceab185d002631d7d846
tak074 <tak074@ucsd.edu>
2021-03-10 20:12:51 -0800

    implemented lowestCommonAncestor recursively

---
+++ leetCode/lowestCommonAncestor.js
@@ -0,0 +1,11 @@
+var lowestCommonAncestor = function(root, p, q) {
+  if (!root) return null;
+  if (root === p || root === q) return root;
+
+  let left = lowestCommonAncestor(root.left, p, q);
+  let right = lowestCommonAncestor(root.right, p, q);
+
+  if (left && right) return root;
+  if (left && !right) return left;
+  else return right;
+};
