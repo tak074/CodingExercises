ef5c2df3aaee1e91b7fe9d621202c3bf60157e6e
tak074 <tak074@ucsd.edu>
2021-03-10 21:44:09 -0800

    implemented searchMatrix but very inefficiently. needs to revise

--- leetCode/search2DMatrixII.js
+++ leetCode/search2DMatrixII.js
@@ -13,3 +13,25 @@ const check = function(row, col, matrix, target) {
 
   return right || down;
 }
+
+
+
+
+var searchMatrix = function(matrix, target) {
+  let binarySearch = function (start, end, nums) {
+    if (nums[start] === target || nums[end] === target) return true;
+    if (start >= end) return false;
+    let mid = Math.floor((start + end) / 2);
+    if (nums[mid] === target || nums[start] === target || nums[end] === target) return true;
+    if (nums[mid] > target) return binarySearch(start, mid - 1, nums);
+    if (nums[mid] < target) return binarySearch(mid + 1, end, nums);
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][0] <= target && matrix[i][matrix[i].length - 1] >= target) {
+      if (binarySearch(0, matrix[i].length - 1, matrix[i])) return true;
+    }
+  }
+
+  return false;
+};
