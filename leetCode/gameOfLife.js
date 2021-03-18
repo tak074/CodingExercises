5dab8710807e3efb4b2328ebb599bb97783c2994
tak074 <tak074@ucsd.edu>
2021-03-18 16:08:15 -0700

    time complexity did not change but space complexity has improved

--- leetCode/gameOfLife.js
+++ leetCode/gameOfLife.js
@@ -1,14 +1,12 @@
 var gameOfLife = function(board) {
   // keep track of changes
-  let changes = new Map();
-
   let countLives = function(row, col) {
     let counter = 0;
     for (let j = -1; j < 2; j++) {
       if (board[row + j] !== undefined) {
         for (let i = -1; i < 2; i++) {
           if (j === 0 && i === 0) continue;
-          if (board[row + j][col + i] === 1) counter++;
+          if (board[row + j][col + i] >= 1) counter++;
         }
       }
     }
@@ -23,22 +21,19 @@ var gameOfLife = function(board) {
       let lives = countLives(i, j);
       // if currCell === 1
       if (board[i][j] === 1) {
-        // check underPopulation
-        if (lives < 2) changes.set([i, j], 0);
-        // check overPopulation
-        if (lives > 3) changes.set([i, j], 0);
+        // check underPopulation and overPopulation
+        if (lives < 2 || lives > 3) board[i][j] = 2;
       } else { // if currCell === 0
         // check reproduction
-        if (lives === 3) changes.set([i, j], 1);
+        if (lives === 3) board[i][j] = -1;
       }
     }
   }
-  // loop through changes
-  changes.forEach((val, key) => {
-    // make changes.
-    board[key[0]][key[1]] = val;
-  })
 
-  // return board
-  return board;
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[i][j] === 2) board[i][j] = 0;
+      if (board[i][j] === -1) board[i][j] = 1;
+    }
+  }
 };
