166a8745cea7a6d43ac4ceee8f5d49545302c303
tak074 <tak074@ucsd.edu>
2021-03-27 12:53:14 -0700

    implemented longestCommonPrefix

---
+++ leetCode/longestCommonPrefix.js
@@ -0,0 +1,19 @@
+var longestCommonPrefix = function(strs) {
+  if (strs.length === 0) return "";
+  let pre = '';
+  let match = true;
+
+  for (let i = 0; i < strs[0].length; i++) {
+    let currLet = strs[0][i];
+    for (let j = 0; j < strs.length; j++) {
+      if (strs[j][i] !== currLet) {
+        match = false;
+        break;
+      }
+    }
+    if (!match) break;
+    pre += currLet;
+  }
+
+  return pre;
+};
